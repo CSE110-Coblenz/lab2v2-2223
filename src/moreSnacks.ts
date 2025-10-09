f5502d1e1f1b373d980f1ed93d8e3eedd5969931
Ridhi <rsrikanth@ucsd.edu>
2025-10-09 15:06:06 -0700

    new snakcs added

--- src/moreSnacks.ts
+++ src/moreSnacks.ts
@@ -1,7 +1,7 @@
 const snacks: string[] = [];
 
-snacks.push("Oranges");
-snacks.push("Tomatoes");
+snacks.push("Cookies");
+snacks.push("Chips");
 
 function print() {
   for(let i = 0; i < snacks.length; i++) {
