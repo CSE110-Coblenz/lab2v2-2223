8ae8eee73c7f56f7fbad18e201d10f787b660032
Ridhi <rsrikanth@ucsd.edu>
2025-10-09 15:16:44 -0700

    new main

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
-const table = require('./table');
+const snacks: string[] = [];
 
-table.log();
+snacks.push("Cookies");
+snacks.push("Chips");
+
+function print() {
+  for(let i = 0; i < snacks.length; i++) {
+    console.log(snacks[i]);
+  }
+}
+
+print();
