b362587b8df600328aa071e3914015cf026e9f93
sheep <aflockosheep@gmail.com>
2025-10-09 15:56:18 -0700

    added zucchini and cucumber

--- src/snacks.ts
+++ src/snacks.ts
@@ -2,6 +2,9 @@ const snacks: string[] = [];
 
 snacks.push("Oranges");
 snacks.push("Tomatoes");
+snacks.push("Zucchini");
+snacks.push("Cucumber");
+
 
 function print() {
   for(let i = 0; i < snacks.length; i++) {
