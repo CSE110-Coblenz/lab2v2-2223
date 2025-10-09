a9be0e478fdac63db0a6d8d35791cdf42f102057
Jaehyun Cha <83200506+KR-JaehyunCha@users.noreply.github.com>
2025-10-09 15:53:19 -0700

    Added all final music

--- src/musics.ts
+++ src/musics.ts
@@ -3,6 +3,7 @@ const music: string[] = [];
 music.push("Indie Pop");
 music.push("Rock");
 music.push("Jazz");
+music.push("Classical");
 
 function listMusics(music: string[]): void {
     console.log("Available music genres: " + music.join(", "));
