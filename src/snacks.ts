ec2b274e0bad583e4148f65e1c611b1e222f1964
sheep <aflockosheep@gmail.com>
2025-10-09 15:56:18 -0700

    added zucchini and cucumber

--- src/snacks.ts
+++ src/snacks.ts
@@ -5,6 +5,9 @@ const funct = require('./animation');
 funct.anim();
 snacks.push("Oranges");
 snacks.push("Tomatoes");
+snacks.push("Zucchini");
+snacks.push("Cucumber");
+
 
 function print() {
   for(let i = 0; i < snacks.length; i++) {
