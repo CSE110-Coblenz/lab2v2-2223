d938390278b71595fe5945303309ba54b6bd9699
sheep <aflockosheep@gmail.com>
2025-10-09 14:26:06 -0700

    Added src/snacks.ts implemented a list that stores snacks and a function to print the snacks

---
+++ src/snacks.ts
@@ -0,0 +1,12 @@
+const snacks: string[] = [];
+
+snacks.push("Oranges");
+snacks.push("Tomatoes");
+
+function print() {
+  for(let i = 0; i < snacks.length; i++) {
+    console.log(snacks[i]);
+  }
+}
+
+print();
