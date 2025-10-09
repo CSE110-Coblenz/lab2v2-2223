a78b502d8a7745a562516541ebf341d65bcd2150
sheep <aflockosheep@gmail.com>
2025-10-09 15:59:03 -0700

    added pumpkin added chocolate

--- src/snacks.ts
+++ src/snacks.ts
@@ -7,7 +7,8 @@ snacks.push("Oranges");
 snacks.push("Tomatoes");
 snacks.push("Zucchini");
 snacks.push("Cucumber");
-
+snacks.push("pumpkin");
+snacks.push("chocolate");
 
 function print() {
   for(let i = 0; i < snacks.length; i++) {
