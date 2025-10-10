c49ce0791a6c46549ec100694e84b34301af84b3
Jaehyun Cha <83200506+KR-JaehyunCha@users.noreply.github.com>
2025-10-09 17:47:53 -0700

    Added all final guest

--- src/guestlist.ts
+++ src/guestlist.ts
@@ -3,6 +3,9 @@ const guestlist: string[] = [];
 guestlist.push("Tom");
 guestlist.push("Jerry");
 guestlist.push("Mickey");
+guestlist.push("Minnie");
+guestlist.push("Jake");
+guestlist.push("Finn");
 
 function listguest(guestlist: string[]): void {
     console.log("Guest List: " + guestlist.join(", "));
